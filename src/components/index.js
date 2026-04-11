d6db80fb99afafb960c56cf0061841e967bb4f11
Bhanu Mogusala <bhanumogusala@gmail.com>
2026-04-11 10:18:37 -0400

    chore: established project folder structure

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as CustomerCard } from './CustomerCard';
+export { default as ErrorMessage } from './ErrorMessage';
+export { default as LoadingSpinner } from './LoadingSpinner';
+export { default as MonthlyBreakdown } from './MonthlyBreakdown';
+export { default as RewardsTable } from './RewardsTable';
